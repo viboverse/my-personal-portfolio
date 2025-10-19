7fe2cabda258988d089de7be07c818608452c70b
Vibo <vahab.afsharian94@gmail.com>
2025-10-19 12:53:53 +0300

    Update: Add new git certifcate

--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -1,70 +1,5 @@
 import CertificateItem from "@/components/certificate-item";
-
-const CERTIFICATES = [
-   {
-      id: 1,
-      title: "React - The Complete Guide 2025",
-      imageUrl: "/certificates/react-udemy.jpg",
-      issuer: "Udmey",
-      issuerUrl: "https://www.udemy.com/",
-
-      issueDate: "July 2025",
-   },
-   {
-      id: 2,
-      title: "Next.js 15 - The Complete Guide",
-      imageUrl: "/certificates/nextjs-udemy.jpg",
-      issuer: "Udmey",
-      issuerUrl: "https://www.udemy.com/",
-
-      issueDate: "September 2025",
-   },
-   {
-      id: 3,
-      title: "TypeScript 5+",
-      imageUrl: "/certificates/typescript-v4-frontendmasters.jpg",
-      issuer: "Frontend Masters",
-      issuerUrl: "https://frontendmasters.com/",
-
-      issueDate: "August 2025",
-   },
-   {
-      id: 4,
-      title: "React and TypeScript",
-      imageUrl: "/certificates/react-typescript-v2-frontendmasters.jpg",
-      issuer: "Frontend Masters",
-      issuerUrl: "https://frontendmasters.com/",
-
-      issueDate: "August 2025",
-   },
-   {
-      id: 5,
-      title: "Tailwind CSS 4+",
-      imageUrl: "/certificates/tailwind-css-v2-frontendmasters.jpg",
-      issuer: "Frontend Masters",
-      issuerUrl: "https://frontendmasters.com/",
-
-      issueDate: "September 2025",
-   },
-   {
-      id: 6,
-      title: " HTML5",
-      imageUrl: "/certificates/html-coursera.jpg",
-      issuer: "Coursera",
-      issuerUrl: "https://www.coursera.org/",
-
-      issueDate: "October 2021",
-   },
-   {
-      id: 7,
-      title: "CSS Foundations",
-      imageUrl: "/certificates/css-foundation-frontendmasters.jpg",
-      issuer: "Frontend Masters",
-      issuerUrl: "https://frontendmasters.com/",
-
-      issueDate: "July 2025",
-   },
-];
+import { CERTIFICATES } from "@/data/certificates";
 
 export default function CertificatePage() {
    return (
@@ -81,13 +16,11 @@ export default function CertificatePage() {
          </div>
 
          {/* Fixed grid */}
-         {/* <div className="mx-auto max-w-7xl"> */}
          <div className="grid justify-items-center gap-x-4 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
             {CERTIFICATES.map((certificate) => (
                <CertificateItem key={certificate.id} {...certificate} />
             ))}
          </div>
-         {/* </div> */}
       </section>
    );
 }
---
+++ src/data/certificates.ts
@@ -0,0 +1,74 @@
+export const CERTIFICATES = [
+   {
+      id: 1,
+      title: "React - The Complete Guide 2025",
+      imageUrl: "/certificates/react-udemy.jpg",
+      issuer: "Udmey",
+      issuerUrl: "https://www.udemy.com/",
+
+      issueDate: "July 2025",
+   },
+   {
+      id: 2,
+      title: "Next.js 15 - The Complete Guide",
+      imageUrl: "/certificates/nextjs-udemy.jpg",
+      issuer: "Udmey",
+      issuerUrl: "https://www.udemy.com/",
+
+      issueDate: "September 2025",
+   },
+   {
+      id: 3,
+      title: "TypeScript 5+",
+      imageUrl: "/certificates/typescript-v4-frontendmasters.jpg",
+      issuer: "Frontend Masters",
+      issuerUrl: "https://frontendmasters.com/",
+
+      issueDate: "August 2025",
+   },
+   {
+      id: 4,
+      title: "React and TypeScript",
+      imageUrl: "/certificates/react-typescript-v2-frontendmasters.jpg",
+      issuer: "Frontend Masters",
+      issuerUrl: "https://frontendmasters.com/",
+
+      issueDate: "August 2025",
+   },
+   {
+      id: 5,
+      title: "Tailwind CSS 4+",
+      imageUrl: "/certificates/tailwind-css-v2-frontendmasters.jpg",
+      issuer: "Frontend Masters",
+      issuerUrl: "https://frontendmasters.com/",
+
+      issueDate: "September 2025",
+   },
+   {
+      id: 6,
+      title: " HTML5",
+      imageUrl: "/certificates/html-coursera.jpg",
+      issuer: "Coursera",
+      issuerUrl: "https://www.coursera.org/",
+
+      issueDate: "October 2021",
+   },
+   {
+      id: 7,
+      title: "CSS Foundations",
+      imageUrl: "/certificates/css-foundation-frontendmasters.jpg",
+      issuer: "Frontend Masters",
+      issuerUrl: "https://frontendmasters.com/",
+
+      issueDate: "July 2025",
+   },
+   {
+      id: 8,
+      title: "Everything You'll Need to Know About Git",
+      imageUrl: "/certificates/everything-git.jpg",
+      issuer: "Frontend Masters",
+      issuerUrl: "https://frontendmasters.com/",
+
+      issueDate: "October 2025",
+   },
+];
